37511e41d0de799214db02ae356351b809f3c48e
shaniaking <shaniaking65@gmail.com>
2025-06-14 14:51:46 -0400

    app.py
    Added block of code at the end for Render deployment

--- sales-engagement-api/app.py
+++ sales-engagement-api/app.py
@@ -60,3 +60,9 @@ def home():
         <br>
         {prediction_result}
     """
+#for Render
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
